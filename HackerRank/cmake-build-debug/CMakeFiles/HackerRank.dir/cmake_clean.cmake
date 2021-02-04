feef0856da40d00a4b9a55c75842cbf9f19ccff2
tapixhx <tapish818@gmail.com>
2021-02-04 13:45:43 +0530

    CMake

---
+++ HackerRank/cmake-build-debug/CMakeFiles/HackerRank.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HackerRank.dir/Problem1.cpp.obj"
+  "CMakeFiles/HackerRank.dir/between-two-set.cpp.obj"
+  "CMakeFiles/HackerRank.dir/bill-division.cpp.obj"
+  "CMakeFiles/HackerRank.dir/breaking-best-and-worst-records.cpp.obj"
+  "CMakeFiles/HackerRank.dir/cats-and-a-mouse.cpp.obj"
+  "CMakeFiles/HackerRank.dir/designer-pdf-viewer.cpp.obj"
+  "CMakeFiles/HackerRank.dir/diivisible-sum-pairs.cpp.obj"
+  "CMakeFiles/HackerRank.dir/main.cpp.obj"
+  "CMakeFiles/HackerRank.dir/subarray-division.cpp.obj"
+  "CMakeFiles/HackerRank.dir/the-hurdle-race.cpp.obj"
+  "HackerRank.exe"
+  "HackerRank.exe.manifest"
+  "HackerRank.pdb"
+  "libHackerRank.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/HackerRank.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
