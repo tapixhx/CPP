3f2ac75829588a11c0b77a2ba2d625601cb10664
tapixhx <tapish818@gmail.com>
2021-02-04 19:32:42 +0530

    Migratory Birds

---
+++ HackerRank/migratory-birds.cpp
@@ -0,0 +1,21 @@
+//https://www.hackerrank.com/challenges/migratory-birds/problem
+//#include <bits/stdc++.h>
+//using namespace std;
+//
+//int main() {
+//    int n, max=0, ans, x;
+//    vector<int> a(5,0);
+//    cin >> n;
+//    for(int i=0; i<n; i++) {
+//        cin >> x;
+//        a[x-1] += 1;
+//    }
+//    for(int i=0; i<5; i++) {
+//        if(a[i] > max) {
+//            max=a[i];
+//            ans=i+1;
+//        }
+//    }
+//    cout << ans;
+//    return 0;
+//}
