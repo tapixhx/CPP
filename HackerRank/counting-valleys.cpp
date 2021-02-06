73bd3fd9a788a20e0d2e5bc8fc1f90988ad938ba
tapixhx <tapish818@gmail.com>
2021-02-07 00:03:56 +0530

    Counting valleys

---
+++ HackerRank/counting-valleys.cpp
@@ -0,0 +1,19 @@
+//https://www.hackerrank.com/challenges/counting-valleys/problem
+//#include <bits/stdc++.h>
+//using namespace std;
+//
+//int main() {
+//    int n, c=0, ans=0;
+//    string p;
+//    cin >> n >> p;
+//    for(int i=0; i<n; i++) {
+//        if(c==0 && p[i]=='D')
+//            ans++;
+//        if(p[i]=='D')
+//            c-=1;
+//        if(p[i]=='U')
+//            c+=1;
+//    }
+//    cout << (ans);
+//    return 0;
+//}
