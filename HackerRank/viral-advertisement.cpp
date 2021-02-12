10f08cd3a9cce47259e0fa05dbf550088ac14fca
tapixhx <tapish818@gmail.com>
2021-02-13 02:35:16 +0530

    Viral advertisement

---
+++ HackerRank/viral-advertisement.cpp
@@ -0,0 +1,16 @@
+//https://www.hackerrank.com/challenges/strange-advertising/problem
+//#include <bits/stdc++.h>
+//using namespace std;
+//
+//int main() {
+//    int s=5, l=0, t, n;
+//    cin >> n;
+//    for(int i=1; i<=n; i++) {
+//        t=s/2;
+//        l+=t;
+//        t*=3;
+//        s=t;
+//    }
+//    cout << l;
+//    return 0;
+//}
