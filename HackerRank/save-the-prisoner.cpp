496d3b04daa598a1a7a76876cfc088b53a155003
tapixhx <tapish818@gmail.com>
2021-02-13 15:06:41 +0530

    Save the prisoner

---
+++ HackerRank/save-the-prisoner.cpp
@@ -0,0 +1,16 @@
+//https://www.hackerrank.com/challenges/save-the-prisoner/problem
+//#include <bits/stdc++.h>
+//using namespace std;
+//
+//int main() {
+//    int n, m, s, t, l;
+//    cin >> t;
+//    while(t--) {
+//        cin >> n >> m >> s;
+//        l = (s+m-1)%n;
+//        if(l==0)
+//            l=n;
+//        cout << l << "\n";
+//    }
+//    return 0;
+//}
