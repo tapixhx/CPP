8898380c4812ee41bc6b29ac6f2eacd86b4fa92c
tapixhx <tapish818@gmail.com>
2021-02-14 14:37:06 +0530

    Circular array rotation

---
+++ HackerRank/circular-array-rotation.cpp
@@ -0,0 +1,20 @@
+//https://www.hackerrank.com/challenges/circular-array-rotation/problem
+//#include <bits/stdc++.h>
+//using namespace std;
+//
+//int main() {
+//    int n, k, q, t, m;
+//    cin >> n >> k >> q;
+//    vector<int> a(n), b(n);
+//    for(int i=0; i<n; i++)
+//        cin >> a[i];
+//    for(int i=0;i<n;i++){
+//        b[(i+k)%n]=a[i];
+//    }
+//    while(q--){
+//        int in;
+//        cin >> in;
+//        cout << b[in] << "\n";
+//    }
+//    return 0;
+//}
