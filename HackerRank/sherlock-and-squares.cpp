db0d9ac9644895dae99bd7670648fddf6038a371
tapixhx <tapish818@gmail.com>
2021-02-19 18:43:36 +0530

    Sherlock and squares

---
+++ HackerRank/sherlock-and-squares.cpp
@@ -0,0 +1,13 @@
+//https://www.hackerrank.com/challenges/sherlock-and-squares/problem
+//#include<bits/stdc++.h>
+//using namespace std;
+//
+//int main() {
+//    int t, a, b, k, ans=0;
+//    cin >> t;
+//    while(t--) {
+//        cin >> a >> b;
+//        cout << floor(sqrt(b)) - ceil(sqrt(a)) + 1 << "\n";
+//    }
+//    return 0;
+//}
