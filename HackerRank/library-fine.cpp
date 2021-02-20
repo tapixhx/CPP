68d6ee02cf2fdf2d0041c241583ffc81d4a7eb38
tapixhx <tapish818@gmail.com>
2021-02-20 09:15:23 +0530

    Library fine

---
+++ HackerRank/library-fine.cpp
@@ -0,0 +1,15 @@
+//https://www.hackerrank.com/challenges/library-fine/problem
+//#include<bits/stdc++.h>
+//using namespace std;
+//
+//int main() {
+//    int a, b, c, d, e, f;
+//    cin >> a >> b >> c >> d >> e >> f;
+//    if(c==f && e==b)
+//        cout << 15*abs(a-d);
+//    if(f==c && e!=b)
+//        cout << 500*abs(e-b);
+//    if(f!=c)
+//        cout << 10000;
+//    return 0;
+//}
