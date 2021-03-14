2a3d8d7cde22b9a30a319d6ed3b35fdef11c7889
tapixhx <tapish818@gmail.com>
2021-03-14 23:43:42 +0530

    Repeated Strings

--- HackerRank/main.cpp
+++ HackerRank/main.cpp
@@ -1,4 +1,5 @@
 #include<bits/stdc++.h>
+typedef long long ll;
 using namespace std;
 
 int main() {
---
+++ HackerRank/repeated-strings.cpp
@@ -0,0 +1,26 @@
+//https://www.hackerrank.com/challenges/repeated-string/problem
+//#include<bits/stdc++.h>
+//typedef long long ll;
+//using namespace std;
+//
+//int main() {
+//    ll l, n, r, ans, c=0;
+//    string s;
+//    cin >> s;
+//    cin >> n;
+//    l=s.length();
+//    for(int i=0; i<s.length(); i++)
+//        if(s[i] == 'a')
+//            c++;
+//    if(n%l == 0)
+//        cout << c*(n/l);
+//    else {
+//        ans = c*(n/l);
+//        r=n%l;
+//        for(int i=0; i<r; i++)
+//            if(s[i] == 'a')
+//                ans++;
+//        cout << ans;
+//    }
+//    return 0;
+//}
