862ab359762efc7f9b2d3a11d9eee2536a06a28b
tapixhx <tapish818@gmail.com>
2021-03-15 21:50:14 +0530

    Jumping on the clouds

---
+++ HackerRank/jumping-on-the-cloud.cpp
@@ -0,0 +1,24 @@
+//https://www.hackerrank.com/challenges/jumping-on-the-clouds/problem
+//#include<bits/stdc++.h>
+//typedef long long ll;
+//using namespace std;
+//
+//int main() {
+//    int n, a[100], f=0, ans=0;
+//    cin >> n;
+//    for(int i=0; i<n; i++)
+//        cin >> a[i];
+//    while(f<n) {
+//        if(f+2 < n) {
+//            if (a[f + 2] == 0)
+//                f += 2;
+//            else
+//                f += 1;
+//        }
+//        else
+//            f+=1;
+//        ans++;
+//    }
+//    cout << ans-1;
+//    return 0;
+//}
