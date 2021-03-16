b1b5fa59a118f98ed820af077349e3c7a57f2f0f
tapixhx <tapish818@gmail.com>
2021-03-16 19:05:08 +0530

    Equalize the array

---
+++ HackerRank/equalize-the-array.cpp
@@ -0,0 +1,16 @@
+//https://www.hackerrank.com/challenges/equality-in-a-array/problem
+//#include<bits/stdc++.h>
+//typedef long long ll;
+//using namespace std;
+//
+//int main() {
+//    int a[101] = {0}, m=0, n, x;
+//    cin >> n;
+//    for(int i=0; i<n; i++) {
+//        cin >> x;
+//        a[x]++;
+//        m = max(m, a[x]);
+//    }
+//    cout << n-m;
+//    return 0;
+//}
