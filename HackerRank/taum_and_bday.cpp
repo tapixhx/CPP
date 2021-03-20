5d988ca93412276be8bf659f5cdf36e5fe06522a
tapixhx <tapish818@gmail.com>
2021-03-20 22:37:04 +0530

    Taum and Bday

---
+++ HackerRank/taum_and_bday.cpp
@@ -0,0 +1,19 @@
+//https://www.hackerrank.com/challenges/taum-and-bday/problem
+//#include<bits/stdc++.h>
+//typedef long long ll;
+//using namespace std;
+//
+//int main() {
+//    ll t, b, w, bc, wc, z;
+//    cin >> t;
+//    while(t--) {
+//        cin >> b >> w >> bc >> wc >> z;
+//        if(bc > wc && bc > wc+z)
+//            cout << (w+b)*wc+b*z << "\n";
+//        else if(wc > bc && wc > bc+z)
+//            cout << (b+w)*bc+w*z << "\n";
+//        else
+//            cout << bc*b+wc*w << "\n";
+//    }
+//    return 0;
+//}
