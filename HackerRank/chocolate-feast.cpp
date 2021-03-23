53873455b1d8cc55f27646fd17f2e920ed2f70f6
tapixhx <tapish818@gmail.com>
2021-03-23 22:59:26 +0530

    Chocolate feast

---
+++ HackerRank/chocolate-feast.cpp
@@ -0,0 +1,23 @@
+//https://www.hackerrank.com/challenges/chocolate-feast/problem
+//#include<bits/stdc++.h>
+//typedef long long ll;
+//using namespace std;
+//
+//int main() {
+//    int t, n, m, c, ans, temp, d, rem;
+//    cin >> t;
+//    while(t--) {
+//        cin >> n >> c >> m;
+//        ans=n/c;
+//        temp=ans;
+//        d=ans;
+//        while(d!=0) {
+//            d=temp/m;
+//            rem=temp%m;
+//            temp=rem+d;
+//            ans+=d;
+//        }
+//        cout << ans << "\n";
+//    }
+//    return 0;
+//}
