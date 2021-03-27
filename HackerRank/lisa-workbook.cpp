5ff5224990d2e7b2e35d8c783e0d6fc4e98d4e4f
tapixhx <tapish818@gmail.com>
2021-03-27 17:02:02 +0530

    Lisa's workbook

---
+++ HackerRank/lisa-workbook.cpp
@@ -0,0 +1,24 @@
+//https://www.hackerrank.com/challenges/lisa-workbook/problem
+//#include<bits/stdc++.h>
+//typedef long long ll;
+//using namespace std;
+//
+//int main() {
+//    int n, k, np, sp=0, pn=0;
+//    cin >> n >> k;
+//    for(int i=0;i<n;i++) {
+//        cin >> np;
+//        pn++;
+//        int problem=1;
+//        while(np>0) {
+//            np--;
+//            if(problem==pn)
+//                sp++;
+//            if(problem%k==0 && np!=0)
+//                pn++;
+//            problem++;
+//        }
+//    }
+//    cout << sp;
+//    return 0;
+//}
