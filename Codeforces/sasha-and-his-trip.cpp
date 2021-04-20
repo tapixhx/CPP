ef63d638490fb5236b8ff8dfaa2cae6c2ce33dfe
tapixhx <tapish818@gmail.com>
2021-04-20 23:24:26 +0530

    Sasha and his trip

---
+++ Codeforces/sasha-and-his-trip.cpp
@@ -0,0 +1,47 @@
+//#include <bits/stdc++.h>
+//
+//#define ll              long long int
+//#define vii             vector<int>
+//#define vll             vector<ll>
+//#define mii             map<int, int>
+//#define mll             map<ll, ll>
+//#define sii             set<int>
+//#define sll             set<ll>
+//#define umi             unordered_map<int, int>
+//#define uml             unordered_map<ll, ll>
+//#define usi             unordered_set<int>
+//#define usl             unordered_set<ll>
+//#define pii             pair<int, int>
+//#define pll             pair<ll, ll>
+//#define pb              push_back
+//#define pf              push_front
+//#define mk              make_pair
+//#define endl            "\n"
+//#define desc            greater<int>()
+//#define F               first
+//#define S               second
+//#define mod             1000000007
+//#define pi              3.141592653589793238
+//#define lcm(a,b)        (a/(__gcd(a,b)))*b
+//#define FASTIO          ios_base::sync_with_stdio(false);cin.tie(NULL);cout<<fixed;cout.precision(10);
+//
+//using namespace std;
+//
+//int main() {
+//    FASTIO
+//    int n, v, d, num=2, ans;
+//    cin >> n >> v;
+//    if(n>v) {
+//        d=(n-1)-v;
+//        ans=v;
+//        for(int i=1; i<=d; i++) {
+//            ans+=num;
+//            num++;
+//        }
+//        cout << ans;
+//    }
+//    else {
+//        cout << n-1;
+//    }
+//    return 0;
+//}
