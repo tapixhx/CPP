2c0ae6281ea622bf5fe15433d3e7cb1dbf9a2756
tapixhx <tapish818@gmail.com>
2021-04-30 00:11:42 +0530

    Domino on wndowsill

---
+++ Codeforces/domino-on-windowsill.cpp
@@ -0,0 +1,26 @@
+//#include <bits/stdc++.h>
+//
+//#define ll              long long int
+//#define mod             1000000007
+//#define pi              3.141592653589793238
+//#define lcm(a,b)        (a/(__gcd(a,b)))*b
+//#define FASTIO          ios_base::sync_with_stdio(false);cin.tie(NULL);cout<<fixed;cout.precision(10);
+//
+//using namespace std;
+//
+//int main() {
+//    FASTIO
+//    int t;
+//    cin >> t;
+//    while(t--) {
+//        int n, k1, k2, w, b, w1, b1;
+//        cin >> n >> k1 >> k2 >> w >> b;
+//        w1 = k1+k2;
+//        b1 = 2*n-w1;
+//        if(w1-2*w>=0 && b1-2*b>=0)
+//            cout << "YES\n";
+//        else
+//            cout << "NO\n";
+//    }
+//    return 0;
+//}
