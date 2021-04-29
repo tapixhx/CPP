80027445d316ac0e280619f65cd324eba7aa384c
tapixhx <tapish818@gmail.com>
2021-04-29 13:02:56 +0530

    Strange table

---
+++ Codeforces/strange-table.cpp
@@ -0,0 +1,25 @@
+//#include <bits/stdc++.h>
+//
+//#define ll              long long int
+//#define mod             1000000007
+//#define pi              3.141592653589793238
+//#define lcm(a,b)        (a/(__gcd(a,b)))*b
+//#define FASTIO          ios_base::sync_with_stdio(false);cin.tie(NULL);cout<<fixed;cout.precision(10);
+//
+//using namespace std;
+//
+//int main() {
+//    FASTIO
+//    ll t;
+//    cin >> t;
+//    while(t--) {
+//        ll n, m, x, r, c, ans;
+//        cin >> n >> m >> x;
+//        x-=1;
+//        c = x/n;
+//        r = x%n;
+//        ans = (r*m)+c+1;
+//        cout << ans << "\n";
+//    }
+//    return 0;
+//}
