996f66c3a4c947d0c426c447435340b5b3bb0d18
tapixhx <tapish818@gmail.com>
2021-04-29 21:28:00 +0530

    The cake is a lie

---
+++ Codeforces/the-cake-is-a-lie.cpp
@@ -0,0 +1,25 @@
+//#include <bits/stdc++.h>
+//
+//#define ll              long long int
+//#define mod             1000000007
+//#define pi              3.141592653589793238
+//#define lcm(a,b)        (a/(__gcd(a,b)))*b
+//#define FASTIO          ios_base::sync_with_stdio(false);cin.tie(NULL);cout<<fixed;cout.precision(10);
+//
+//using namespace std;
+//
+//int main() {
+//    FASTIO
+//    int t;
+//    cin >> t;
+//    while(t--) {
+//        int n, m, k, cal;
+//        cin >> n >> m >> k;
+//        cal = (n-1) + n*(m-1);
+//        if(cal == k)
+//            cout << "YES\n";
+//        else
+//            cout << "NO\n";
+//    }
+//    return 0;
+//}
