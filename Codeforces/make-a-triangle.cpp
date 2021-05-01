8522da2ea7b827124f0fb3898b46a929c770c8c1
tapixhx <tapish818@gmail.com>
2021-05-01 21:48:18 +0530

    Make a triangle!

---
+++ Codeforces/make-a-triangle.cpp
@@ -0,0 +1,27 @@
+//#include <bits/stdc++.h>
+//
+//#define ll              long long int
+//#define mod             1000000007
+//#define pi              3.141592653589793238
+//#define lcm(a,b)        (a/(__gcd(a,b)))*b
+//#define FASTIO          ios_base::sync_with_stdio(false);cin.tie(NULL);cout<<fixed;cout.precision(10);
+//
+//using namespace std;
+//
+//int main() {
+//    FASTIO
+//    int a, b, c, d;
+//    cin >> a >> b >> c;
+//    if(a+b < c+1) {
+//        cout << (c+1)-(a+b);
+//    }
+//    else if(a+c < b+1) {
+//        cout << (b+1)-(a+c);
+//    }
+//    else if(b+c < a+1) {
+//        cout << (a+1)-(b+c);
+//    }
+//    else
+//        cout << 0;
+//    return 0;
+//}
