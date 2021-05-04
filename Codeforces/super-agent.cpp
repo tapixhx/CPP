e556c869dace53d1fb2054d9e7c1bb74e1e76ed7
tapixhx <tapish818@gmail.com>
2021-05-04 20:59:10 +0530

    Super agent

---
+++ Codeforces/super-agent.cpp
@@ -0,0 +1,23 @@
+//#include <bits/stdc++.h>
+//
+//#define ll              long long int
+//#define mod             1000000007
+//#define pi              3.141592653589793238
+//#define lcm(a,b)        (a/(__gcd(a,b)))*b
+//#define FASTIO          ios_base::sync_with_stdio(false);cin.tie(NULL);cout<<fixed;cout.precision(10);
+//
+//using namespace std;
+//
+//int main() {
+//    FASTIO
+//    char a[3][3];
+//    for(int i=0; i<3; i++) {
+//        for(int j=0; j<3; j++)
+//            cin >> a[i][j];
+//    }
+//    if((a[0][0] == a[2][2]) && (a[1][0] == a[1][2]) && (a[2][0] == a[0][2]) && (a[0][1] == a[2][1]))
+//        cout << "YES";
+//    else
+//        cout << "NO";
+//    return 0;
+//}
