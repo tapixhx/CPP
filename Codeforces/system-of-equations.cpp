8efc2bef24563786879c3766636b2c43d9bc9b87
tapixhx <tapish818@gmail.com>
2021-05-04 21:10:42 +0530

    System of equations

---
+++ Codeforces/system-of-equations.cpp
@@ -0,0 +1,23 @@
+//#include <bits/stdc++.h>
+//
+//#define ll              long long int
+//#define mod             1000000007
+//#define pi              3.141592653589793238
+//#define lcm(a,b)        (a/(__gcd(a,b)))*b
+//#define FASTIO          ios_base::sync_with_stdio(false);cin.tie(NULL);cout<<fixed;cout.precision(10);
+//
+//using namespace std;
+//
+//int main() {
+//    FASTIO
+//    int n, m, a=0, b, ans=0;
+//    cin >> n >> m;
+//    while(a<=m && a*a<=n) {
+//        b = n-a*a;
+//        if(a+b*b == m)
+//            ans++;
+//        a++;
+//    }
+//    cout << ans;
+//    return 0;
+//}
