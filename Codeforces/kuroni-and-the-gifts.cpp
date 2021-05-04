a02c1795301626be53c5722f248a2d26d19ab522
tapixhx <tapish818@gmail.com>
2021-05-05 00:15:01 +0530

    Kuroni and the gifts

---
+++ Codeforces/kuroni-and-the-gifts.cpp
@@ -0,0 +1,33 @@
+//#include <bits/stdc++.h>
+//
+//#define ll              long long int
+//#define mod             1000000007
+//#define pi              3.141592653589793238
+//#define lcm(a,b)        (a/(__gcd(a,b)))*b
+//#define FASTIO          ios_base::sync_with_stdio(false);cin.tie(NULL);cout<<fixed;cout.precision(10);
+//
+//using namespace std;
+//
+//int main() {
+//    FASTIO
+//    int t;
+//    cin >> t;
+//    while(t--) {
+//        int n;
+//        cin >> n;
+//        vector<int> a(n+1), b(n+1);
+//        for(int i=1; i<=n; i++)
+//            cin >> a[i];
+//        for(int i=1; i<=n; i++)
+//            cin >> b[i];
+//        sort(a.begin(), a.end());
+//        sort(b.begin(), b.end());
+//        for(int i=1; i<=n; i++)
+//            cout << a[i] << " ";
+//        cout << "\n";
+//        for(int i=1; i<=n; i++)
+//            cout << b[i] << " ";
+//        cout << "\n";
+//    }
+//    return 0;
+//}
