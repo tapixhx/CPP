403916ce8bb907c73a679901363588bc46164539
tapixhx <tapish818@gmail.com>
2021-05-05 17:16:24 +0530

    Indian summer

---
+++ Codeforces/indian-summer.cpp
@@ -0,0 +1,24 @@
+//#include <bits/stdc++.h>
+//
+//#define ll              long long int
+//#define mod             1000000007
+//#define pi              3.141592653589793238
+//#define lcm(a,b)        (a/(__gcd(a,b)))*b
+//#define FASTIO          ios_base::sync_with_stdio(false);cin.tie(NULL);cout<<fixed;cout.precision(10);
+//
+//using namespace std;
+//
+//int main() {
+//    FASTIO
+//    int n;
+//    set<string> m;
+//    cin >> n;
+//    cin.ignore();
+//    for(int i=1; i<=n; i++) {
+//        string s;
+//        getline(cin, s);
+//        m.insert(s);
+//    }
+//    cout << m.size();
+//    return 0;
+//}
