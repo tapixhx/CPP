6f94233a1169b9c6969507f9d8cb98f6ba784374
tapixhx <tapish818@gmail.com>
2021-05-08 20:50:58 +0530

    Filling shapes

---
+++ Codeforces/filling-shapes.cpp
@@ -0,0 +1,17 @@
+//#include <bits/stdc++.h>
+//
+//#define ll              long long int
+//#define mod             1000000007
+//#define pi              3.141592653589793238
+//#define lcm(a,b)        (a/(__gcd(a,b)))*b
+//#define FASTIO          ios_base::sync_with_stdio(false);cin.tie(NULL);cout<<fixed;cout.precision(10);
+//
+//using namespace std;
+//
+//int main() {
+//    FASTIO
+//    int n;
+//    cin >> n;
+//    (n%2 != 0) ? cout << 0 : cout << (int)pow(2, n/2);
+//    return 0;
+//}
