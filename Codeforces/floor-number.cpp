2e33ea053be5b9fde5f7f8f380caebbb6fb7e835
tapixhx <tapish818@gmail.com>
2021-05-15 22:39:47 +0530

    Floor number

---
+++ Codeforces/floor-number.cpp
@@ -0,0 +1,25 @@
+//#include <bits/stdc++.h>
+//
+//#define ll              long long int
+//#define mod             1000000007
+//#define pi              3.141592653589793238
+//#define lcm(a,b)        (a/(__gcd(a,b)))*b
+//#define FASTIO          ios_base::sync_with_stdio(false);cin.tie(NULL);cout<<fixed;cout.precision(10);
+//
+//using namespace std;
+//
+//int main() {
+//    FASTIO
+//    int t;
+//    cin >> t;
+//    while(t--) {
+//        int n, x;
+//        cin >> n >> x;
+//        if(n <= 2)
+//            cout << 1 << "\n";
+//        else {
+//            cout << (n-3)/x+2 << "\n";
+//        }
+//    }
+//    return 0;
+//}
