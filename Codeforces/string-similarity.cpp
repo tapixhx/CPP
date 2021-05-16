a808e0d626f6050051d2c9a4d980007c198c75c1
tapixhx <tapish818@gmail.com>
2021-05-16 13:24:07 +0530

    String similarity

---
+++ Codeforces/string-similarity.cpp
@@ -0,0 +1,29 @@
+//#include <bits/stdc++.h>
+//
+//#define ll              long long int
+//#define mod             1000000007
+//#define pi              3.141592653589793238
+//#define lcm(a,b)        (a/(__gcd(a,b)))*b
+//#define FASTIO          ios_base::sync_with_stdio(false);cin.tie(NULL);cout<<fixed;cout.precision(10);
+//
+//using namespace std;
+//
+//int main() {
+//    FASTIO
+//    int t;
+//    cin >> t;
+//    while(t--) {
+//        int n, k=0;
+//        string s, str, ans = "000000000000000000000000000000000000000000000000";
+//        cin >> n >> s;
+//        for(int i=0; i<=n; i++) {
+//            str = s.substr(i, n);
+//            ans[i] = str[i];
+//        }
+//        for(int i=0; i<n; i++) {
+//            cout << ans[i];
+//        }
+//        cout << "\n";
+//    }
+//    return 0;
+//}
