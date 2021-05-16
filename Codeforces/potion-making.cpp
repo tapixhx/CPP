6119bbdbe584949f608422cfbc5c5bfaeaeee135
tapixhx <tapish818@gmail.com>
2021-05-16 15:33:13 +0530

    Potion making

---
+++ Codeforces/potion-making.cpp
@@ -0,0 +1,22 @@
+//#include <bits/stdc++.h>
+//
+//#define ll              long long int
+//#define mod             1000000007
+//#define pi              3.141592653589793238
+//#define lcm(a,b)        (a/(__gcd(a,b)))*b
+//#define FASTIO          ios_base::sync_with_stdio(false);cin.tie(NULL);cout<<fixed;cout.precision(10);
+//
+//using namespace std;
+//
+//int main() {
+//    FASTIO
+//    int t;
+//    cin >> t;
+//    while(t--) {
+//        int k, l;
+//        cin >> k;
+//        l = __gcd(100, 100 - k);
+//        cout << (k / l + ((100 - k) / l)) << "\n";
+//    }
+//    return 0;
+//}
