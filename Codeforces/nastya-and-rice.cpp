4c81de0f555eb90f34886903c2f746c36b86694d
tapixhx <tapish818@gmail.com>
2021-05-19 00:53:30 +0530

    Nastya and rice

---
+++ Codeforces/nastya-and-rice.cpp
@@ -0,0 +1,26 @@
+//#include <bits/stdc++.h>
+//
+//#define ll              long long int
+//#define mod             1000000007
+//#define pi              3.141592653589793238
+//#define lcm(a,b)        (a/(__gcd(a,b)))*b
+//#define FASTIO          ios_base::sync_with_stdio(false);cin.tie(NULL);cout<<fixed;cout.precision(10);
+//
+//using namespace std;
+//
+//int main() {
+//    FASTIO
+//    int t;
+//    cin >> t;
+//    while(t--) {
+//        int n, a, b, c, d;
+//        cin >> n >> a >> b >> c >> d;
+//        int s = (a-b)*n;
+//        int e = (a+b)*n;
+//        if(e<c-d || s>c+d)
+//            cout << "NO\n";
+//        else
+//            cout << "YES\n";
+//    }
+//    return 0;
+//}
