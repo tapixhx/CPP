24341dfb8a6db6229b2b610a7612b6e3ad3fea53
tapixhx <tapish818@gmail.com>
2021-05-19 23:27:56 +0530

    Three strings

---
+++ Codeforces/three-strings.cpp
@@ -0,0 +1,29 @@
+//#include <bits/stdc++.h>
+//
+//#define ll              long long int
+//#define mod             1000000007
+//#define pi              3.141592653589793238
+//#define lcm(a,b)        (a/(__gcd(a,b)))*b
+//#define FASTIO          ios_base::sync_with_stdio(false);cin.tie(NULL);cout<<fixed;cout.precision(10);
+//
+//using namespace std;
+//
+//int main() {
+//    FASTIO
+//    int t;
+//    cin >> t;
+//    while(t--) {
+//        int flag = 0;
+//        string a, b, c;
+//        cin >> a >> b >> c;
+//        int n = a.length();
+//        for(int i=0; i<n; i++) {
+//            if(c[i] != a[i] && c[i] != b[i]) {
+//                flag = 1;
+//                break;
+//            }
+//        }
+//        (flag) ? cout << "NO\n" : cout << "YES\n";
+//    }
+//    return 0;
+//}
