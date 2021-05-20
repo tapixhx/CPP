f2501eebe8d2ae473f9727e97e5a9673dbb1265f
tapixhx <tapish818@gmail.com>
2021-05-20 22:28:44 +0530

    There were k

---
+++ Codeforces/there-were-k.cpp
@@ -0,0 +1,27 @@
+//#include <bits/stdc++.h>
+//
+//#define ll              long long int
+//#define mod             1000000007
+//#define pi              3.141592653589793238
+//#define lcm(a,b)        (a/(__gcd(a,b)))*b
+//#define FASTIO          ios_base::sync_with_stdio(false);cin.tie(NULL);cout<<fixed;cout.precision(10);
+//
+//using namespace std;
+//
+//ll highestPower(ll n) {
+//    ll p = (ll)log2(n);
+//    return (ll)pow(2, p);
+//}
+//
+//int main() {
+//    FASTIO
+//    ll t;
+//    cin >> t;
+//    while(t--) {
+//        ll n, ans;
+//        cin >> n;
+//        ans = highestPower(n)-1;
+//        cout << ans << "\n";
+//    }
+//    return 0;
+//}
