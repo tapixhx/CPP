5754b47db2ab51a2e275a8a591ebf746385fcbf1
tapixhx <tapish818@gmail.com>
2021-05-21 00:02:21 +0530

    Palindrome game (Easy version)

---
+++ Codeforces/palindrome-game-(easy-version).cpp
@@ -0,0 +1,29 @@
+//#include <bits/stdc++.h>
+//
+//#define ll              long long int
+//#define mod             1000000007
+//#define pi              3.141592653589793238
+//#define lcm(a,b)        (a/(__gcd(a,b)))*b
+//#define FASTIO          ios_base::sync_with_stdio(false);cin.tie(NULL);cout<<fixed;cout.precision(10);
+//
+//using namespace std;
+//
+//int main() {
+//    FASTIO
+//    ll t;
+//    cin >> t;
+//    while(t--) {
+//        ll n, c = 0;
+//        string s;
+//        cin >> n >> s;
+//        for(ll i=0; i<n; i++) {
+//            if(s[i] == '0')
+//                c++;
+//        }
+//        if(c%2 != 0 && c != 1)
+//            cout << "ALICE\n";
+//        else
+//            cout << "BOB\n";
+//    }
+//    return 0;
+//}
