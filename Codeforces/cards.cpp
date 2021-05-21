772ee3ff622e2321c74f67f5d6af49cf6622a592
tapixhx <tapish818@gmail.com>
2021-05-21 22:56:58 +0530

    Cards

---
+++ Codeforces/cards.cpp
@@ -0,0 +1,28 @@
+//#include <bits/stdc++.h>
+//
+//#define ll              long long int
+//#define mod             1000000007
+//#define pi              3.141592653589793238
+//#define lcm(a,b)        (a/(__gcd(a,b)))*b
+//#define FASTIO          ios_base::sync_with_stdio(false);cin.tie(NULL);cout<<fixed;cout.precision(10);
+//
+//using namespace std;
+//
+//int main() {
+//    FASTIO
+//    ll m;
+//    string s;
+//    cin >> m >> s;
+//    int n = 0, z = 0;
+//    for(int i=0; i<m; i++) {
+//        if(s[i] == 'n')
+//            n++;
+//        if(s[i] == 'z')
+//            z++;
+//    }
+//    while(n--)
+//        cout << 1 << " ";
+//    while(z--)
+//        cout << 0 << " ";
+//    return 0;
+//}
