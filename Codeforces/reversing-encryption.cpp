515b7b180730e02b8bd8906a85f503501c67744f
tapixhx <tapish818@gmail.com>
2021-05-21 22:04:24 +0530

    Reversing encryption

---
+++ Codeforces/reversing-encryption.cpp
@@ -0,0 +1,22 @@
+//#include <bits/stdc++.h>
+//
+//#define ll              long long int
+//#define mod             1000000007
+//#define pi              3.141592653589793238
+//#define lcm(a,b)        (a/(__gcd(a,b)))*b
+//#define FASTIO          ios_base::sync_with_stdio(false);cin.tie(NULL);cout<<fixed;cout.precision(10);
+//
+//using namespace std;
+//
+//int main() {
+//    FASTIO
+//    int n;
+//    string s;
+//    cin >> n >> s;
+//    for(int i=1; i<=n; i++) {
+//        if(n%i == 0)
+//            reverse(s.begin(), s.begin()+i);
+//    }
+//    cout << s;
+//    return 0;
+//}
