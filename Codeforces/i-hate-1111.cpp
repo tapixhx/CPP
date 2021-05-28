c85c9d0913bd543fde6ed18ba922fb565b548845
tapixhx <tapish818@gmail.com>
2021-05-29 00:41:59 +0530

    I hate 1111

---
+++ Codeforces/i-hate-1111.cpp
@@ -0,0 +1,28 @@
+//#include <bits/stdc++.h>
+//
+//#define ll              long long int
+//#define mod             1000000007
+//#define pi              3.141592653589793238
+//#define lcm(a,b)        (a/(__gcd(a,b)))*b
+//#define FASTIO          ios_base::sync_with_stdio(false);cin.tie(NULL);cout<<fixed;cout.precision(10);
+//
+//using namespace std;
+//
+//int main() {
+//    FASTIO
+//    int t;
+//    cin >> t;
+//    while(t--) {
+//        int n;
+//        cin >> n;
+//        int rem = n%11;
+//        n -= 111 * rem;
+//        if(n>=0 && n%11==0) {
+//            cout << "YES\n";
+//        }
+//        else {
+//            cout << "NO\n";
+//        }
+//    }
+//    return 0;
+//}
