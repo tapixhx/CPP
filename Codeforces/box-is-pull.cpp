9d446b334627549fa4b777bdee86a946f84ab7fd
tapixhx <tapish818@gmail.com>
2021-05-30 20:39:50 +0530

    Box is pull

---
+++ Codeforces/box-is-pull.cpp
@@ -0,0 +1,24 @@
+//#include <bits/stdc++.h>
+//
+//#define ll              long long int
+//#define mod             1000000007
+//#define pi              3.141592653589793238
+//#define lcm(a,b)        (a/(__gcd(a,b)))*b
+//#define FASTIO          ios_base::sync_with_stdio(false);cin.tie(NULL);cout<<fixed;cout.precision(10);
+//
+//using namespace std;
+//
+//int main() {
+//    FASTIO
+//    int t;
+//    cin >> t;
+//    while(t--) {
+//        int u, v, w, x;
+//        cin >> u >> v >> w >> x;
+//        if(u == w || v == x)
+//            cout << abs(u-w) + abs(v-x) << "\n";
+//        else
+//            cout << abs(u-w) + abs(v-x) + 2 << "\n";
+//    }
+//    return 0;
+//}
