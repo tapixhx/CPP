43f4ef02120f45f7c487683fb995c463e4c6cf6b
tapixhx <tapish818@gmail.com>
2021-06-02 09:30:56 +0530

    Chat room

---
+++ Codeforces/chat-room.cpp
@@ -0,0 +1,25 @@
+//#include <bits/stdc++.h>
+//
+//#define ll              long long int
+//#define mod             1000000007
+//#define pi              3.141592653589793238
+//#define lcm(a,b)        (a/(__gcd(a,b)))*b
+//#define FASTIO          ios_base::sync_with_stdio(false);cin.tie(NULL);cout<<fixed;cout.precision(10);
+//
+//using namespace std;
+//
+//int main() {
+//    FASTIO
+//    string s, t="hello";
+//    cin >> s;
+//    ll i, j=0;
+//    for(i=0; i<s.length(); i++) {
+//        if(s[i] == t[j])
+//            j++;
+//    }
+//    if(j == t.length())
+//        cout << "YES";
+//    else
+//        cout << "NO";
+//    return 0;
+//}
