c6ce550f08f542302b8acdaad083bdff5757a249
tapixhx <tapish818@gmail.com>
2021-06-05 22:31:24 +0530

    Young physicist

---
+++ Codeforces/young-physicist.cpp
@@ -0,0 +1,25 @@
+//#include <bits/stdc++.h>
+//
+//#define ll              long long int
+//#define mod             1000000007
+//#define pi              3.141592653589793238
+//#define lcm(a,b)        (a/(__gcd(a,b)))*b
+//#define FASTIO          ios_base::sync_with_stdio(false);cin.tie(NULL);cout<<fixed;cout.precision(10);
+//
+//using namespace std;
+//
+//int main() {
+//    FASTIO
+//    int n;
+//    cin >> n;
+//    int x1 = 0, y1 = 0, z1 = 0;
+//    while(n--) {
+//        int x, y, z;
+//        cin >> x >> y >> z;
+//        x1 += x;
+//        y1 += y;
+//        z1 += z;
+//    }
+//    (!x1 && !y1 && !z1) ? cout << "YES" : cout << "NO";
+//    return 0;
+//}
