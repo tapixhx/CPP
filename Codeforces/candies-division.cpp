bdc853b3d9a75ba904c2662965323c23f3cc2d7e
tapixhx <tapish818@gmail.com>
2021-06-10 21:07:23 +0530

    Candies division

---
+++ Codeforces/candies-division.cpp
@@ -0,0 +1,25 @@
+//#include <bits/stdc++.h>
+//
+//#define ll              long long int
+//#define mod             1000000007
+//#define pi              3.141592653589793238
+//#define lcm(a,b)        (a/(__gcd(a,b)))*b
+//#define FASTIO          ios_base::sync_with_stdio(false);cin.tie(NULL);cout<<fixed;cout.precision(10);
+//
+//using namespace std;
+//
+//int main() {
+//    FASTIO
+//    ll t;
+//    cin >> t;
+//    while(t--) {
+//        ll n, k;
+//        cin >> n >> k;
+//        ll r = n/k;
+//        ll p = n%k;
+//        ll c = k/2;
+//        ll ans = (r*k)+min(p,c);
+//        cout << ans << "\n";
+//    }
+//    return 0;
+//}
