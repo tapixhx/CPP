9ffa7d5ee5e5e6bb220b5a45911f288e03fc8837
tapixhx <tapish818@gmail.com>
2021-06-15 21:25:00 +0530

    Juicer

---
+++ Codeforces/juicer.cpp
@@ -0,0 +1,26 @@
+//#include <bits/stdc++.h>
+//
+//#define ll              long long int
+//#define mod             1000000007
+//#define pi              3.141592653589793238
+//#define lcm(a,b)        (a/(__gcd(a,b)))*b
+//#define FASTIO          ios_base::sync_with_stdio(false);cin.tie(NULL);cout<<fixed;cout.precision(10);
+//
+//using namespace std;
+//
+//int main() {
+//    FASTIO
+//    ll n, m, max, i, c=0, sum=0, x;
+//    cin >> n >> m >> max;
+//    for(i=0; i<n; i++) {
+//        cin >> x;
+//        if(x<=m)
+//            sum=sum+x;
+//        if(sum>max) {
+//            c++;
+//            sum=0;
+//        }
+//    }
+//    cout << c;
+//    return 0;
+//}
