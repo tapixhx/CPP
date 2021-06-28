5f8b6af667cdb5f3fe300ed08ee2f0e415676d27
tapixhx <tapish818@gmail.com>
2021-06-28 12:12:22 +0530

    Jeff and digits

---
+++ Codeforces/jeff-and-digits.cpp
@@ -0,0 +1,25 @@
+//#include <bits/stdc++.h>
+//
+//#define ll              long long int
+//#define mod             1000000007
+//#define pi              3.141592653589793238
+//#define lcm(a,b)        (a/(__gcd(a,b)))*b
+//#define FASTIO          ios_base::sync_with_stdio(false);cin.tie(NULL);cout<<fixed;cout.precision(10);
+//
+//using namespace std;
+//
+//int main() {
+//    FASTIO
+//    int n, f = 0, z = 0, k;
+//    cin >> n;
+//    while(n--) cin>>k, k==5 ? f++ : z++; f=f/9*9;
+//    if(f==0||z<1) {
+//        cout << (z<1 ? "-1" : "0");
+//        return 0;
+//    }
+//    while(f--)
+//        cout<<'5';
+//    while(z--)
+//        cout<<'0';
+//    return 0;
+//}
