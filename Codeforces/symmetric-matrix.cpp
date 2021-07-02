9a2c7ceb1878a4a828ba053259f7ff118e20a37c
tapixhx <tapish818@gmail.com>
2021-07-02 23:03:59 +0530

    Symmetric matrix

---
+++ Codeforces/symmetric-matrix.cpp
@@ -0,0 +1,34 @@
+//#include <bits/stdc++.h>
+//
+//#define ll              long long int
+//#define mod             1000000007
+//#define pi              3.141592653589793238
+//#define lcm(a,b)        (a/(__gcd(a,b)))*b
+//#define FASTIO          ios_base::sync_with_stdio(false);cin.tie(NULL);cout<<fixed;cout.precision(10);
+//
+//using namespace std;
+//
+//int main() {
+//    FASTIO
+//    ll t;
+//    cin >> t;
+//    while(t--) {
+//        ll n, i, f = 0, m;
+//        cin >> n >> m;
+//        for (i=0; i<n; i++) {
+//            ll a, b, c, d;
+//            cin >> a >> b >> c >> d;
+//            if (b == c)
+//                f = 1;
+//        }
+//        if (m % 2) {
+//            cout << "NO\n";
+//            continue;
+//        }
+//        if (f)
+//            cout << "YES\n";
+//        else
+//            cout << "NO\n";
+//    }
+//    return 0;
+//}
