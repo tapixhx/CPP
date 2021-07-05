13493f56505d48d5e9fa2b6fed277f886aa5dc5c
tapixhx <tapish818@gmail.com>
2021-07-05 21:07:01 +0530

    Candies

---
+++ Codeforces/candies.cpp
@@ -0,0 +1,29 @@
+//#include <bits/stdc++.h>
+//
+//#define ll              long long int
+//#define mod             1000000007
+//#define pi              3.141592653589793238
+//#define lcm(a,b)        (a/(__gcd(a,b)))*b
+//#define FASTIO          ios_base::sync_with_stdio(false);cin.tie(NULL);cout<<fixed;cout.precision(10);
+//
+//using namespace std;
+//
+//int main() {
+//    FASTIO
+//    ll t;
+//    cin >> t;
+//    while(t--) {
+//        ll n, i, s = 3;
+//        cin >> n;
+//        i = 2;
+//        while(1) {
+//            if(n%s==0) {
+//                cout << n/s << "\n";
+//                break;
+//            }
+//            s=s+pow(2,i);
+//            i++;
+//        }
+//    }
+//    return 0;
+//}
