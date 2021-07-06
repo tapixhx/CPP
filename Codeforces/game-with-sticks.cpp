04c74667cce655dff43e3f651e32f63032ee5f9b
tapixhx <tapish818@gmail.com>
2021-07-07 01:18:42 +0530

    Game with sticks

---
+++ Codeforces/game-with-sticks.cpp
@@ -0,0 +1,20 @@
+//#include <bits/stdc++.h>
+//
+//#define ll              long long int
+//#define mod             1000000007
+//#define pi              3.141592653589793238
+//#define lcm(a,b)        (a/(__gcd(a,b)))*b
+//#define FASTIO          ios_base::sync_with_stdio(false);cin.tie(NULL);cout<<fixed;cout.precision(10);
+//
+//using namespace std;
+//
+//int main() {
+//    FASTIO
+//    int n, m;
+//    cin >> n >> m;
+//    int mx = max(n, m);
+//    int k = n*m;
+//    int x = k/mx;
+//    (x%2 == 0) ? cout<<"Malvika" : cout<<"Akshat";
+//    return 0;
+//}
