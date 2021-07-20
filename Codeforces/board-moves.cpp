3f28a22e4368c31e0778cc00f578ac1adab18cf6
tapixhx <tapish818@gmail.com>
2021-07-20 15:07:35 +0530

    Board moves

---
+++ Codeforces/board-moves.cpp
@@ -0,0 +1,23 @@
+//#include <bits/stdc++.h>
+//
+//#define ll              long long int
+//#define mod             1000000007
+//#define pi              3.141592653589793238
+//#define lcm(a,b)        (a/(__gcd(a,b)))*b
+//#define FASTIO          ios_base::sync_with_stdio(false);cin.tie(NULL);cout<<fixed;cout.precision(10);
+//
+//using namespace std;
+//
+//int main() {
+//    FASTIO
+//    ll t;
+//    cin >> t;
+//    while(t--) {
+//        ll n, res = 0;
+//        cin >> n;
+//        for(ll i=1; i<=n/2; i++)
+//            res += i*i;
+//        cout << res*8 << "\n";
+//    }
+//    return 0;
+//}
