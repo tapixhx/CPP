241dd8917cd139da3d7d92ba1d90a4157277c8a7
tapixhx <tapish818@gmail.com>
2021-07-20 20:52:57 +0530

    Olesya and rodion

---
+++ Codeforces/olesya-and-rodion.cpp
@@ -0,0 +1,30 @@
+//#include <bits/stdc++.h>
+//
+//#define ll              long long int
+//#define mod             1000000007
+//#define pi              3.141592653589793238
+//#define lcm(a,b)        (a/(__gcd(a,b)))*b
+//#define FASTIO          ios_base::sync_with_stdio(false);cin.tie(NULL);cout<<fixed;cout.precision(10);
+//
+//using namespace std;
+//
+//int main() {
+//    FASTIO
+//    int n, t;
+//    cin >> n >> t;
+//    if(t < 10) {
+//        while(n--)
+//            cout << t;
+//    }
+//    else {
+//        if(n != 1) {
+//            n -= 1;
+//            cout << 2;
+//            while(n--)
+//                cout << 0;
+//        }
+//        else
+//            cout << -1;
+//    }
+//    return 0;
+//}
