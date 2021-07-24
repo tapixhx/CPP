477cdd3b435b38f4af6d393859fbe0b4fbdb570a
tapixhx <tapish818@gmail.com>
2021-07-24 12:30:18 +0530

    Dreamoon and stairs

---
+++ Codeforces/dreamoon-and-stairs.cpp
@@ -0,0 +1,25 @@
+//#include <bits/stdc++.h>
+//
+//#define ll              long long int
+//#define mod             1000000007
+//#define pi              3.141592653589793238
+//#define lcm(a,b)        (a/(__gcd(a,b)))*b
+//#define FASTIO          ios_base::sync_with_stdio(false);cin.tie(NULL);cout<<fixed;cout.precision(10);
+//
+//using namespace std;
+//
+//int main() {
+//    FASTIO
+//    int n, m;
+//    cin >> n >> m;
+//    int flag = 0;
+//    int i = (n+1)/2;
+//    for(; i<=n; i++) {
+//        if(i%m == 0) {
+//            flag = 1;
+//            break;
+//        }
+//    }
+//    (flag) ? cout << i : cout << -1;
+//    return 0;
+//}
