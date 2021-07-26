a0bb9114097b247fc0c14ad2182b1c7482dbdb63
tapixhx <tapish818@gmail.com>
2021-07-27 00:23:04 +0530

    New year transportation

---
+++ Codeforces/new-year-transportation.cpp
@@ -0,0 +1,26 @@
+//#include <bits/stdc++.h>
+//
+//#define ll              long long int
+//#define mod             1000000007
+//#define pi              3.141592653589793238
+//#define lcm(a,b)        (a/(__gcd(a,b)))*b
+//#define FASTIO          ios_base::sync_with_stdio(false);cin.tie(NULL);cout<<fixed;cout.precision(10);
+//
+//using namespace std;
+//
+//int main() {
+//    FASTIO
+//    ll n, t, i;
+//    cin >> n >> t;
+//    int a[n+1];
+//    for(i=1; i<n; i++)
+//        cin >> a[i];
+//    i=1;
+//    while(i<t)
+//        i=i+a[i];
+//    if(i==t)
+//        cout << "YES";
+//    else
+//        cout << "NO";
+//    return 0;
+//}
