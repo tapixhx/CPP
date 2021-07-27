58a1e0aa9ba45613336198afba64b07eef0df3ab
tapixhx <tapish818@gmail.com>
2021-07-27 17:02:54 +0530

    Distance and axis

---
+++ Codeforces/distance-and-axis.cpp
@@ -0,0 +1,28 @@
+//#include <bits/stdc++.h>
+//
+//#define ll              long long int
+//#define mod             1000000007
+//#define pi              3.141592653589793238
+//#define lcm(a,b)        (a/(__gcd(a,b)))*b
+//#define FASTIO          ios_base::sync_with_stdio(false);cin.tie(NULL);cout<<fixed;cout.precision(10);
+//
+//using namespace std;
+//
+//int main() {
+//    FASTIO
+//    int t;
+//    cin >> t;
+//    while(t--) {
+//        ll a, k;
+//        cin >> a >> k;
+//        if (k > a)
+//            cout << k - a << "\n";
+//        else {
+//            if ((k % 2) != (a % 2))
+//                cout << "1\n";
+//            else
+//                cout << "0\n";
+//        }
+//    }
+//    return 0;
+//}
