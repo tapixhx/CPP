f1a36ee5cae5c49f4a04b4aab4e716e00bc0efed
tapixhx <tapish818@gmail.com>
2021-07-28 21:28:49 +0530

    Little artem

---
+++ Codeforces/little-artem.cpp
@@ -0,0 +1,25 @@
+//#include <bits/stdc++.h>
+//
+//#define ll              long long int
+//#define mod             1000000007
+//#define pi              3.141592653589793238
+//#define lcm(a,b)        (a/(__gcd(a,b)))*b
+//#define FASTIO          ios_base::sync_with_stdio(false);cin.tie(NULL);cout<<fixed;cout.precision(10);
+//
+//using namespace std;
+//
+//int main() {
+//    FASTIO
+//    int t;
+//    cin >> t;
+//    while(t--) {
+//        int n, m;
+//        cin >> n >> m;
+//        string black_row(m, 'B');
+//        vector<string> result(n, black_row);
+//        result[0][0] = 'W';
+//        for (int i=0; i<n; i++)
+//            cout << result[i] << '\n';
+//    }
+//    return 0;
+//}
