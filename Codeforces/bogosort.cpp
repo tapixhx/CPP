f937d2ea399d5d0b9ab5042014b82c2485884f4b
tapixhx <tapish818@gmail.com>
2021-07-31 12:16:39 +0530

    Bogosort

---
+++ Codeforces/bogosort.cpp
@@ -0,0 +1,27 @@
+//#include <bits/stdc++.h>
+//
+//#define ll              long long int
+//#define mod             1000000007
+//#define pi              3.141592653589793238
+//#define lcm(a,b)        (a/(__gcd(a,b)))*b
+//#define FASTIO          ios_base::sync_with_stdio(false);cin.tie(NULL);cout<<fixed;cout.precision(10);
+//
+//using namespace std;
+//
+//int main() {
+//    FASTIO
+//    int t;
+//    cin >> t;
+//    while(t--) {
+//        int n;
+//        cin >> n;
+//        vector<int> a(n);
+//        for(int &i : a)
+//            cin >> i;
+//        sort(a.begin(), a.end(), greater<int>());
+//        for(int i=0; i<n; i++)
+//            cout << a[i] << " ";
+//        cout << "\n";
+//    }
+//    return 0;
+//}
