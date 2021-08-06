0388a5e469df5e007ed19dd08827e10379621f84
tapixhx <tapish818@gmail.com>
2021-08-06 22:00:32 +0530

    Regular bracket sequence

---
+++ Codeforces/regular-bracket-sequence.cpp
@@ -0,0 +1,21 @@
+//#include <bits/stdc++.h>
+//
+//#define ll              long long int
+//#define mod             1000000007
+//#define pi              3.141592653589793238
+//#define lcm(a,b)        (a/(__gcd(a,b)))*b
+//#define FASTIO          ios_base::sync_with_stdio(false);cin.tie(NULL);cout<<fixed;cout.precision(10);
+//
+//using namespace std;
+//
+//int main() {
+//    FASTIO
+//    int t;
+//    cin >> t;
+//    while(t--) {
+//        string s;
+//        cin >> s;
+//        (s.length() % 2 == 0 and s[0] != ')' and s[s.length()-1] != '(') ? cout << "YES\n" : cout << "NO\n";
+//    }
+//    return 0;
+//}
