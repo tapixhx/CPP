7388c7f78b715a04c4d5d5465310d6f87b3575f0
tapixhx <tapish818@gmail.com>
2021-08-11 16:40:55 +0530

    Cableway

---
+++ Codeforces/cableway.cpp
@@ -0,0 +1,18 @@
+//#include <bits/stdc++.h>
+//
+//#define ll              long long int
+//#define mod             1000000007
+//#define pi              3.141592653589793238
+//#define lcm(a,b)        (a/(__gcd(a,b)))*b
+//#define FASTIO          ios_base::sync_with_stdio(false);cin.tie(NULL);cout<<fixed;cout.precision(10);
+//
+//using namespace std;
+//
+//int main() {
+//    FASTIO
+//    int r, g, b;
+//    cin >> r >> g >> b;
+//    int ans = max(3*((r + 1) / 2) + 27, max(3*((g + 1) / 2 )+ 28, 3*((b + 1) / 2 )+ 29));
+//    cout <<  ans;
+//    return 0;
+//}
