080a1d86144db97843714e1386f3b561d17728f6
tapixhx <tapish818@gmail.com>
2021-08-14 14:20:16 +0530

    Forgotten episode

---
+++ Codeforces/forgotten-episode.cpp
@@ -0,0 +1,28 @@
+//#include <bits/stdc++.h>
+//
+//#define ll              long long int
+//#define mod             1000000007
+//#define pi              3.141592653589793238
+//#define lcm(a,b)        (a/(__gcd(a,b)))*b
+//#define FASTIO          ios_base::sync_with_stdio(false);cin.tie(NULL);cout<<fixed;cout.precision(10);
+//
+//using namespace std;
+//
+//int main() {
+//    FASTIO
+//    int n;
+//    cin >> n;
+//    int a[100001] = {0};
+//    for(int i=0; i<n-1; i++) {
+//        int x;
+//        cin >> x;
+//        a[x]++;
+//    }
+//    for(int i=1; i<=n; i++) {
+//        if(a[i] == 0) {
+//            cout << i;
+//            break;
+//        }
+//    }
+//    return 0;
+//}
