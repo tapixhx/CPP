5047b41e5606a8e8597ec715bfc95df6fee8fa28
tapixhx <tapish818@gmail.com>
2021-08-16 17:27:19 +0530

    VirtualCon3

---
+++ Codeforces/even-but-not-even.cpp
@@ -0,0 +1,34 @@
+//#include <bits/stdc++.h>
+//
+//#define ll              long long int
+//#define mod             1000000007
+//#define pi              3.141592653589793238
+//#define lcm(a,b)        (a/(__gcd(a,b)))*b
+//#define FASTIO          ios_base::sync_with_stdio(false);cin.tie(NULL);cout<<fixed;cout.precision(10);
+//
+//using namespace std;
+//
+//int main() {
+//    FASTIO
+//    int t;
+//    cin >> t;
+//    while(t--) {
+//        int n;
+//        cin >> n;
+//        string s, ans;
+//        cin >> s;
+//        bool found = false;
+//        for(int i=0; i<n; i++) {
+//            if((int)s[i] % 2 != 0) {
+//                ans += s[i];
+//                if(ans.length() == 2) {
+//                    found = true;
+//                    break;
+//                }
+//            }
+//        }
+//        (found) ? cout << ans : cout << -1;
+//        cout << "\n";
+//    }
+//    return 0;
+//}
