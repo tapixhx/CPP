83451bbe6a3ac9da7c6e7fe98d3467c3f3993bec
tapixhx <tapish818@gmail.com>
2021-08-22 21:13:30 +0530

    Maximum cost deletion

---
+++ Codeforces/maximum-cost-deletion.cpp
@@ -0,0 +1,24 @@
+//#include <bits/stdc++.h>
+//
+//#define ll              long long int
+//#define mod             1000000007
+//#define pi              3.141592653589793238
+//#define lcm(a,b)        (a/(__gcd(a,b)))*b
+//#define FASTIO          ios_base::sync_with_stdio(false);cin.tie(NULL);cout<<fixed;cout.precision(10);
+//
+//using namespace std;
+//
+//int main() {
+//    FASTIO
+//    int t;
+//    cin >> t;
+//    while (t--) {
+//        int n, a, b;
+//        string s;
+//        cin >> n >> a >> b >> s;
+//        int m = unique(s.begin(), s.end()) - s.begin();
+//        int k = max(n * b, (m / 2 + 1) * b);
+//        cout << n * a + k << '\n';
+//    }
+//    return 0;
+//}
