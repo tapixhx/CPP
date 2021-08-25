8fcc8bee3e5484e54c8a6e199bf825daace3d504
tapixhx <tapish818@gmail.com>
2021-08-25 22:08:45 +0530

    Two rabbits

---
+++ Codeforces/two-rabbits.cpp
@@ -0,0 +1,24 @@
+//#include <bits/stdc++.h>
+//
+//#define ll              long long int
+//#define mod             1000000007
+//#define pi              3.141592653589793238
+//#define lcm(a,b)        (a/(__gcd(a,b)))*b
+//#define FASTIO          ios_base::sync_with_stdio(false);cin.tie(NULL);cout<<fixed;cout.precision(10);
+//
+//using namespace std;
+//
+//int main() {
+//    FASTIO
+//    ll t;
+//    cin >> t;
+//    while(t--) {
+//        ll x, y, a, b;
+//        cin >> x >> y >> a >> b;
+//        if((y - x) % (a + b) == 0)
+//            cout << (y - x) / (a + b) << "\n";
+//        else
+//            cout << -1 << "\n";
+//    }
+//    return 0;
+//}
