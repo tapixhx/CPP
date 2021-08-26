2e449a5cf900b1c93c5489cda3c5a5b61f923a19
tapixhx <tapish818@gmail.com>
2021-08-26 21:01:24 +0530

    Dislike of threes

---
+++ Codeforces/dislike-of-threes.cpp
@@ -0,0 +1,31 @@
+//#include <bits/stdc++.h>
+//
+//#define ll              long long int
+//#define mod             1000000007
+//#define pi              3.141592653589793238
+//#define lcm(a,b)        (a/(__gcd(a,b)))*b
+//#define FASTIO          ios_base::sync_with_stdio(false);cin.tie(NULL);cout<<fixed;cout.precision(10);
+//
+//using namespace std;
+//
+//vector<int> v;
+//
+//void solve() {
+//    for(int i=1;i<=2000;i++) {
+//        if(i%10 != 3 && i%3 != 0)
+//            v.push_back(i);
+//    }
+//}
+//
+//int main() {
+//    FASTIO
+//    solve();
+//    int t;
+//    cin >> t;
+//    while(t--) {
+//        int n;
+//        cin >> n;
+//        cout << v[n-1] << "\n";
+//    }
+//    return 0;
+//}
