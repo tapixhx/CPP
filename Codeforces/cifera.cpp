5b354c54d505e9ea750ae3013301f1e84b2f9f6a
tapixhx <tapish818@gmail.com>
2021-09-02 20:31:49 +0530

    Cifera

---
+++ Codeforces/cifera.cpp
@@ -0,0 +1,27 @@
+//#include <bits/stdc++.h>
+//
+//#define ll              long long int
+//#define mod             1000000007
+//#define pi              3.141592653589793238
+//#define lcm(a,b)        (a/(__gcd(a,b)))*b
+//#define FASTIO          ios_base::sync_with_stdio(false);cin.tie(NULL);cout<<fixed;cout.precision(10);
+//
+//using namespace std;
+//
+//int main() {
+//    FASTIO
+//    ll a, b, c = 0;
+//    cin >> a >> b;
+//    int d = a;
+//    while(d!=b) {
+//        d *= a;
+//        c++;
+//        if(d > b)
+//            break;
+//    }
+//    if(d == b)
+//        cout << "YES" << "\n" << c;
+//    else
+//        cout << "NO" ;
+//    return 0;
+//}
