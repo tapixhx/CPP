45ed7021e70c21afbcc949f307fa5c6d1748bc2e
tapixhx <tapish818@gmail.com>
2021-09-08 20:56:04 +0530

    Card game

---
+++ Codeforces/card-game.cpp
@@ -0,0 +1,36 @@
+//#include <bits/stdc++.h>
+//
+//#define ll              long long int
+//#define mod             1000000007
+//#define pi              3.141592653589793238
+//#define lcm(a,b)        (a/(__gcd(a,b)))*b
+//#define FASTIO          ios_base::sync_with_stdio(false);cin.tie(NULL);cout<<fixed;cout.precision(10);
+//
+//using namespace std;
+//
+//int main() {
+//    FASTIO
+//    char a, b, c, d, e;
+//    cin >> a >> b >> c >> d >> e;
+//    int x, y;
+//    char f[10] = {'6','7','8','9','T','J','Q','K','A'};
+//    if(c == e) {
+//        for(int i=0; i<9; i++) {
+//            if(b == f[i])
+//                x = i;
+//            if(d == f[i])
+//                y = i;
+//        }
+//        if(x > y)
+//            cout << "YES";
+//        else
+//            cout << "NO";
+//    }
+//    else if(c == a and c != e)
+//        cout << "YES";
+//    else if(c == a and c == e)
+//        cout << "YES";
+//    else
+//        cout << "NO";
+//    return 0;
+//}
