655b648da9f051fb8553f6fb4e950ed8f809a302
tapixhx <tapish818@gmail.com>
2021-09-11 21:25:13 +0530

    Presents

---
+++ Codeforces/presents.cpp
@@ -0,0 +1,25 @@
+//#include <bits/stdc++.h>
+//
+//#define ll              long long int
+//#define mod             1000000007
+//#define pi              3.141592653589793238
+//#define lcm(a,b)        (a/(__gcd(a,b)))*b
+//#define FASTIO          ios_base::sync_with_stdio(false);cin.tie(NULL);cout<<fixed;cout.precision(10);
+//
+//using namespace std;
+//
+//int main() {
+//    FASTIO
+//    int n;
+//    cin >> n;
+//    int a[n+1];
+//    for(int i=1; i<=n; i++)
+//        cin >> a[i];
+//    map<int, int> mp;
+//    for(int i=1; i<=n; i++)
+//        mp.insert(pair<int, int>(a[i], i));
+//    map<int, int>::iterator itr;
+//    for (itr = mp.begin(); itr != mp.end(); ++itr)
+//        cout << itr->second << " ";
+//    return 0;
+//}
